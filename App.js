e3ba8d917b71870e207af70c8cbd713e2d5b9d0c
nozky <norvillie.villaruel@edu.sait.ca>
2021-03-08 11:45:50 -0700

    first init

---
+++ App.js
@@ -0,0 +1,43 @@
+
+// images folder = put all you image in here.
+// webp-converted = is where all images put when it's done converting.
+// Relative path, in the terminal run -> 'npm start'
+
+
+
+const webp=require('webp-converter');
+const fs = require('fs');
+const path = require('path');
+
+
+// create a relative file
+const imagesDir = path.join(__dirname,'./images');
+
+// set quality
+const quality = '80'
+
+// Scan directory for images and convert
+const scanDir = ()=>{
+  fs.readdir(imagesDir,(err,files)=>{
+    if(err){
+      console.log(err)
+    }
+
+    files.forEach( file => {
+      // call convert
+      webpConvert(file)
+    })
+  })
+}
+
+// function to convert any imag to webp
+const webpConvert = (file)=>{
+  // q='80' -> 1-100 webp image compression percentage.
+const result = webp.cwebp(`${imagesDir}/${file}`,`./webp-converted/${file.split('.')[0] }.webp` ,`-q ${quality}`,logging="-v");
+result.then((response) => {
+  console.log(response);
+});
+}
+
+// execute scan directory and convert to webp
+scanDir()
---
+++ README.md
@@ -0,0 +1,28 @@
+# Convert any images to webP image in batch.
+---
+
+### Instruction
+
+- Setup
+  - clone this project
+  - run npm install
+
+- Execution
+  - Put any images in the images folder
+  - In a relative path, run 'npm start'
+
+That's it!
+
+--- 
+## Attribution
+
+[https://github.com/scionoftech/webp-converter](https://github.com/scionoftech/webp-converter)
+
+[https://developers.google.com/speed/webp](https://developers.google.com/speed/webp)
+
+---
+## Author
+Norvillie Villaruel
+SAIT web dev 2021
+
+
