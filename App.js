7d33954a076d5772313a6aed098bed1b04f5f141
nozky <norvillie.villaruel@edu.sait.ca>
2021-03-09 01:44:59 -0700

    fix

--- App.js
+++ App.js
@@ -3,8 +3,6 @@
 // webp-converted = is where all images place when it's done converting.
 // Relative path, in the terminal run -> 'npm start'
 
-
-
 const webp = require('webp-converter');
 const fs = require('fs');
 const path = require('path');
@@ -13,31 +11,32 @@ const path = require('path');
 // create a relative path
 const imagesDir = path.join(__dirname,'./images');
 
+
+
 // set quality
 const quality = '80'
 
 // Scan directory for images and convert
-const scanDir = ()=>{
+const scanDir = (imagesDir)=>{
+  console.log(`Selected folder ${imagesDir}`)
   fs.readdir(imagesDir,(err,files)=>{
     if(err){
       console.log(err)
     }
-
     files.forEach( file => {
       // call convert
-      webpConvert(file)
+      webpConvert(file,imagesDir)
     })
-  })
-}
+  })}
 
 // function to convert any imag to webp
-const webpConvert = (file)=>{
+const webpConvert = (file,dir)=>{
   // q='80' -> 1-100 webp image compression percentage.
-const result = webp.cwebp(`${imagesDir}/${file}`,`./webp-converted/${file.split('.')[0] }.webp` ,`-q ${quality}`,logging="-v");
+const result = webp.cwebp(`${dir}/${file}`,`./webp-converted/${file.split('.')[0] }.webp` ,`-q ${quality}`,logging="-v");
 result.then((response) => {
   console.log(response);
 });
 }
 
-// execute scan directory and convert to webp
-scanDir()
+// run
+scanDir(imagesDir);
